0d748b526bd73ee18155278fbc62ce5b4dd52d0f
Rzavsky-ev <rzavsky.ev@gmail.com>
2024-11-13 09:57:00 +0300

    Убрал у велосипеда метод проверки двигателя

--- src/Bicycle.java
+++ src/Bicycle.java
@@ -1,10 +1,5 @@
 public class Bicycle extends Transport {
     @Override
-    public void checkEngine() {
-        System.out.println("У велосипеда нет двигателя.");
-    }
-
-    @Override
     public void checkTrailer() {
         System.out.println("У велосипеда нет прицепа.");
     }
